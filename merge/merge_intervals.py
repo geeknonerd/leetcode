55a2b8c0ccb1815f8f866d901c319a07947701c2
geeknonerd <geek@best2geek.com>
2021-11-07 15:45:17 +0800

    solution id 56 question

---
+++ merge/merge_intervals.py
@@ -0,0 +1,57 @@
+"""
+56. 合并区间
+数组 排序
+中等
+
+
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+
+ 
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2：
+
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ 
+
+提示：
+
+1 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti <= endi <= 10^4
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-intervals
+"""
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    result = solution.merge([[1, 3], [2, 6], [8, 10], [15, 18]])
+    print(result)
+    assert result == [[1, 6], [8, 10], [15, 18]]
+
+    result = solution.merge([[1, 4], [4, 5]])
+    print(result)
+    assert result == [[1, 5]]
