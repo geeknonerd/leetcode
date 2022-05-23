cf7acefac13060cee9be5e14452df525262ddd2a
geeknonerd <geek@best2geek.com>
2022-05-23 11:41:12 +0800

    solution id 175 question

---
+++ db/combine_two_tables.sql
@@ -0,0 +1,75 @@
+-- 175. 组合两个表
+-- 数据库
+-- 简单
+--
+--
+-- 表: Person
+--
+-- +-------------+---------+
+-- | 列名         | 类型     |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- personId 是该表的主键列。
+-- 该表包含一些人的 ID 和他们的姓和名的信息。
+--  
+--
+-- 表: Address
+--
+-- +-------------+---------+
+-- | 列名         | 类型    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- addressId 是该表的主键列。
+-- 该表的每一行都包含一个 ID = PersonId 的人的城市和州的信息。
+--  
+--
+-- 编写一个SQL查询来报告 Person 表中每个人的姓、名、城市和州。如果 personId 的地址不在 Address 表中，则报告为空  null 。
+--
+-- 以 任意顺序 返回结果表。
+--
+-- 查询结果格式如下所示。
+--
+--  
+--
+-- 示例 1:
+--
+-- 输入:
+-- Person表:
+-- +----------+----------+-----------+
+-- | personId | lastName | firstName |
+-- +----------+----------+-----------+
+-- | 1        | Wang     | Allen     |
+-- | 2        | Alice    | Bob       |
+-- +----------+----------+-----------+
+-- Address表:
+-- +-----------+----------+---------------+------------+
+-- | addressId | personId | city          | state      |
+-- +-----------+----------+---------------+------------+
+-- | 1         | 2        | New York City | New York   |
+-- | 2         | 3        | Leetcode      | California |
+-- +-----------+----------+---------------+------------+
+-- 输出:
+-- +-----------+----------+---------------+----------+
+-- | firstName | lastName | city          | state    |
+-- +-----------+----------+---------------+----------+
+-- | Allen     | Wang     | Null          | Null     |
+-- | Bob       | Alice    | New York City | New York |
+-- +-----------+----------+---------------+----------+
+-- 解释:
+-- 地址表中没有 personId = 1 的地址，所以它们的城市和州返回 null。
+-- addressId = 1 包含了 personId = 2 的地址信息。
+--
+-- 来源：力扣（LeetCode）
+-- 链接：https://leetcode.cn/problems/combine-two-tables
+-- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+SELECT p.firstName, p.lastName, a.city, a.state FROM Person AS p
+LEFT OUTER JOIN Address AS a ON p.personId = a.personId
