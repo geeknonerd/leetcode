3362d22b1594e475a2b0798f06d466187d1c08f9
geeknonerd <geek@best2geek.com>
2022-08-12 22:40:01 +0800

    solution id 414 question by go

---
+++ go/sort/third_maximum_number/third_maximum_number.go
@@ -0,0 +1,68 @@
+/*
+414. 第三大的数
+数组 排序
+简单
+
+
+给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+
+
+
+示例 1：
+
+输入：[3, 2, 1]
+输出：1
+解释：第三大的数是 1 。
+示例 2：
+
+输入：[1, 2]
+输出：2
+解释：第三大的数不存在, 所以返回最大的数 2 。
+示例 3：
+
+输入：[2, 2, 3, 1]
+输出：1
+解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。
+
+
+提示：
+
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+
+
+进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/third-maximum-number
+*/
+package main
+
+import (
+	"leetcode/go/utils"
+	"math"
+)
+
+func thirdMax(nums []int) int {
+	a, b, c := math.MinInt64, math.MinInt64, math.MinInt64
+	for _, num := range nums {
+		if num > a {
+			a, b, c = num, a, b
+		} else if a > num && num > b {
+			b, c = num, b
+		} else if b > num && num > c {
+			c = num
+		}
+	}
+	if c == math.MinInt64 {
+		return a
+	}
+	return c
+}
+
+func main() {
+	utils.Assert(thirdMax([]int{3, 2, 1}), 1)
+	utils.Assert(thirdMax([]int{1, 2}), 2)
+	utils.Assert(thirdMax([]int{2, 2, 3, 1}), 1)
+}
