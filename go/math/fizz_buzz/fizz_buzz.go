4fb32e9d80ac9ed09c18172532dc5a2e4304068e
geeknonerd <geek@best2geek.com>
2022-08-13 10:33:23 +0800

    solution id 412 question by go

---
+++ go/math/fizz_buzz/fizz_buzz.go
@@ -0,0 +1,63 @@
+/*
+412. Fizz Buzz
+数学 字符串 模拟
+简单
+
+
+给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+
+answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+answer[i] == "Fizz" 如果 i 是 3 的倍数。
+answer[i] == "Buzz" 如果 i 是 5 的倍数。
+answer[i] == i （以字符串形式）如果上述条件全不满足。
+
+
+示例 1：
+
+输入：n = 3
+输出：["1","2","Fizz"]
+示例 2：
+
+输入：n = 5
+输出：["1","2","Fizz","4","Buzz"]
+示例 3：
+
+输入：n = 15
+输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+提示：
+
+1 <= n <= 10^4
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/fizz-buzz
+*/
+package main
+
+import (
+	"leetcode/go/utils"
+	"strconv"
+)
+
+func fizzBuzz(n int) []string {
+	var res []string
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			res = append(res, "FizzBuzz")
+		} else if i%3 == 0 {
+			res = append(res, "Fizz")
+		} else if i%5 == 0 {
+			res = append(res, "Buzz")
+		} else {
+			res = append(res, strconv.Itoa(i))
+		}
+	}
+	return res
+}
+
+func main() {
+	utils.Assert(fizzBuzz(3), []string{"1", "2", "Fizz"})
+	utils.Assert(fizzBuzz(5), []string{"1", "2", "Fizz", "4", "Buzz"})
+	utils.Assert(fizzBuzz(15), []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"})
+}
