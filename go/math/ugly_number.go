c190ad7aaae107740ebb368e7a0a7bc8af315ecc
geeknonerd <geek@best2geek.com>
2022-09-02 08:53:00 +0800

    solution id 263 question by go

---
+++ go/math/ugly_number.go
@@ -0,0 +1,60 @@
+/*
+263. 丑数
+数学
+简单
+
+
+丑数 就是只包含质因数 2、3 和 5 的正整数。
+
+给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+
+
+
+示例 1：
+
+输入：n = 6
+输出：true
+解释：6 = 2 × 3
+示例 2：
+
+输入：n = 1
+输出：true
+解释：1 没有质因数，因此它的全部质因数是 {2, 3, 5} 的空集。习惯上将其视作第一个丑数。
+示例 3：
+
+输入：n = 14
+输出：false
+解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+
+
+提示：
+
+-2^31 <= n <= 2^31 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/ugly-number
+*/
+package main
+
+import (
+	"leetcode/go/utils"
+)
+
+func isUgly(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	factors := []int{2, 3, 5}
+	for _, factor := range factors {
+		for n%factor == 0 {
+			n /= factor
+		}
+	}
+	return n == 1
+}
+
+func main() {
+	utils.Assert(isUgly(6), true)
+	utils.Assert(isUgly(1), true)
+	utils.Assert(isUgly(14), false)
+}
